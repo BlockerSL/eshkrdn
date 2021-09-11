e63171062f89d6bf1f11498d9538ec7a5928c0c0
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-09-11 18:58:43 +0300

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -121,10 +121,4 @@ h1 {
   padding: 30px;
 }
 
-input{
-  border:1px solid white;
-  border-radius: 15px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-  
-}
 </style>
